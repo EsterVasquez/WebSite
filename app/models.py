b89713e4ba082b0745213e843e3198e70d60a6c7
Diego Jacinto <diego.jacinto.ibarra@gmail.com>
2025-07-18 07:25:19 -0600

    Add initial models for User, Message, BotResponse, Booking, and Service

--- app/models.py
+++ app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    phone_number = models.CharField(max_length=15, unique=True)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    last_command = models.CharField(max_length=50, blank=True, null=True)
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class BotResponse(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    response = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    service = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, default='pending') # e.g., pending, confirmed, cancelled
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=100)
+    link = models.URLField()
+    anticipated_time = models.PositiveIntegerField()    # In days
+    valid_hours_start = models.TimeField()  # Start hour, e.g., 09:00
+    valid_hours_end = models.TimeField(null=True, blank=True)    # End hour, e.g., 17:00 - Optional
+    valid_days = models.CharField(max_length=20)    #e.g, Lun, Mar...Vie
+    valid_date = models.DateField() # Start date, e.g., 2023-01-01
