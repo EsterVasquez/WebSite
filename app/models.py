dc6cb5affff2df30d2ff6cb08647a8fa7bc800fd
Diego Jacinto <diego.jacinto.ibarra@gmail.com>
2025-10-14 22:18:32 -0600

    Refactor Service and Booking models: update Service fields for optional values, restructure ServiceTier model, and enhance Booking model with service_tier foreign key and status choices.

--- app/models.py
+++ app/models.py
@@ -20,19 +20,40 @@ class BotResponse(models.Model):
 
 class Service(models.Model):
     name = models.CharField(max_length=100)
-    link = models.URLField()
-    anticipated_time = models.PositiveIntegerField()    # In days
-    valid_hours_start = models.TimeField()  # Start hour, e.g., 09:00
-    # End hour, e.g., 17:00 - Optional
-    valid_hours_end = models.TimeField(null=True, blank=True)
-    valid_days = models.CharField(max_length=20)  # e.g, Lun, Mar...Vie
-    valid_date = models.DateField()  # Start date, e.g., 2023-01-01
+    link = models.URLField(blank=True, null=True)
 
+
+class ServiceTier(models.Model):
+    service = models.ForeignKey(
+        Service, on_delete=models.CASCADE, related_name="tiers")
+    # Ej: "bara", "meh", "hay plata"
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    duration_minutes = models.PositiveIntegerField()
+
+    valid_hours_start = models.TimeField()
+    valid_hours_end = models.TimeField()
+    valid_days = models.JSONField(default=list)  # Ej: ["Mon", "Wed"]
+    # Fecha de inicio de disponibilidad
+    available_from = models.DateField()
+    available_until = models.DateField(
+        null=True, blank=True)  # Fecha final opcional
+
+    def __str__(self):
+        return f"{self.service.name} - {self.name}"
+
+# TODO: Agregar los dias especiales para dias en que hay que agendar en ocasiones extradorinarias
 
 class Booking(models.Model):
+    STATUS_CHOICES = [
+        ('Pendiente', 'Pendiente'),
+        ('Confirmado', 'Confirmado'),
+        ('Cancelado', 'Cancelado'),
+    ]
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    service_tier = models.ForeignKey(ServiceTier, on_delete=models.DO_NOTHING)
     date = models.DateField()
     time = models.TimeField()
-    service = models.ForeignKey(Service, on_delete=models.DO_NOTHING)
-    # e.g., Pendiente, Confirmado, Cancelado
-    status = models.CharField(max_length=20, default='Pendiente')
+    status = models.CharField(
+        max_length=20, choices=STATUS_CHOICES, default='Pendiente')
